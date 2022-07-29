f6b4454b6f202e6b2211ade31949da8e282c9fe7
saurabh5155 <s.a.patel5155@gmail.com>
2022-07-29 11:27:54 +0530

    List Users(Rest API)

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,11 @@ public class UserController {
 	public ResponseEntity<?> saveUser(UserBean userBean) {
 		userDao.signup(userBean);
 		return ResponseEntity.ok(userBean);
+	}
+	
+	@GetMapping("/signup")
+	public ResponseEntity<?> getAllUsers(){
+		List<UserBean> users = userDao.getAllUsers();
+		return ResponseEntity.ok(users);
 	}
 }
--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,6 +1,9 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,5 +19,9 @@ public class UserDao {
 		stmt.update("insert into users (firstname,lastname,email,password,gender,usertype) values (?,?,?,?,?,?)",
 				userBean.getFirstName(), userBean.getLastName(), userBean.getEmail(), userBean.getPassword(),
 				userBean.getGender(), userBean.getUserType());
+	}
+	
+	public List<UserBean> getAllUsers(){
+		return stmt.query("select * from users", new BeanPropertyRowMapper<UserBean>(UserBean.class));
 	}
 }
